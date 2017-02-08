31a66295b7c123255b78677cb7704e00d7ca4c76
mhellar <mark.hellar@gmail.com>
2017-02-07 23:40:22 -0800

    homework!

--- serial_shader2/AnalogInOutSerial/AnalogInOutSerial.ino
+++ serial_shader2/AnalogInOutSerial/AnalogInOutSerial.ino
@@ -21,24 +21,37 @@
 
 // These constants won't change.  They're used to give names
 // to the pins used:
-const int analogInPin = A0;  // Analog input pin that the potentiometer is attached to
+const int analogInPinx = A0;  // Analog input pin that the potentiometer is attached to
+const int analogInPiny = A5;  // Analog input pin that the potentiometer is attached to
+const int triggerPin = 7;  // Digital input pin that the button is attached to
 
-int sensorValue = 0;        // value read from the pot
+int sensorValuex = 0;        // value read from the pot x
+int sensorValuey = 0;        // value read from the pot y
+int trigger = 0;        // value read from the pot y
 
 void setup() {
   // initialize serial communications at 9600 bps:
-  Serial.begin(9600);
+  Serial.begin(115200);
+  pinMode(7, INPUT_PULLUP);
 }
 
 void loop() {
   // read the analog in value:
-  sensorValue = analogRead(analogInPin);
+  sensorValuex = analogRead(analogInPinx);
+  delay(10);
+  sensorValuey = analogRead(analogInPiny);
+  trigger = digitalRead(triggerPin);
 
   // print the results to the serial monitor:
-  Serial.println(sensorValue);
-
+  Serial.print("xval,");
+  Serial.print(sensorValuex);
+  Serial.print(",yval,");
+  Serial.print(sensorValuey);
+  Serial.print(",trigger,");
+  Serial.println(trigger);
+  
   // wait 2 milliseconds before the next loop
   // for the analog-to-digital converter to settle
   // after the last reading:
-  delay(2);
+  delay(10);
 }
