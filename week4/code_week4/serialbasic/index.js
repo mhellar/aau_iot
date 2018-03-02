c7254336455325d115a0d8148ee921c5010f1566
mhellar <mark.hellar@gmail.com>
2018-03-01 22:04:35 -0800

    Week4 commit

--- week4/code_week4/serialbasic/index.js
+++ week4/code_week4/serialbasic/index.js
@@ -1,14 +1,11 @@
-var com = require("serialport");
-
-var serialPort = new com.SerialPort("/dev/cu.usbmodem1421", {
-    baudrate: 9600,
-    parser: com.parsers.readline('\r\n')
-  });
-
-serialPort.on('open',function() {
-  console.log('Port open');
+const SerialPort = require("serialport");
+const Readline = SerialPort.parsers.Readline;
+const port = new SerialPort("/dev/tty.usbmodem1D11741", {
+    baudRate: 9600
 });
 
-serialPort.on('data', function(data) {
-  console.log(data);
+const parser = port.pipe(new Readline({ delimiter: "\r\n" }));
+
+parser.on("data", function(data) {
+    console.log(data);
 });
